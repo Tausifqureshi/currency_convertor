1276709ad088dfe33351713e6f4b74d4f6cce838
Tausifqureshi <tausifqureshi504@gmail.com>
2024-06-14 19:19:12 +0530

    update

--- currency.js
+++ currency.js
@@ -109,25 +109,5 @@ window.addEventListener("load", () => {
 
 
 
-// echo "# currency_convertor" >> README.md
 
 
-// # currency_convertor
-// This was our project for We have completed this project in 2 days.
-// Our target was to complete all main functionality currency exchange website .
-
-// ## Credits
-// - Icons : [Font Awesome](https://fontawesome.com/)
-// - References : [MDN Docs](https://developer.mozilla.org/en-US/) | [W3School](https://www.w3schools.com/)
-
-// ## Tech Stack
-
-// - HTML 5
-// - CSS 3
-// - Javascript
-// - GitHub
-// - API
-
-// ## Authors
-
-// - [@Tausif Qureshi](https://github.com/Tausifqureshi)
